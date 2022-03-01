05f0ec982a793642c0f14d7ab2b32bce44e0af39
Fabu <fabun@amazon.co.jp>
2022-03-01 22:28:42 +0900

    Trying to call something, it became too late... :P

--- src/main/scala/fun/wordle/WordleRoutes.scala
+++ src/main/scala/fun/wordle/WordleRoutes.scala
@@ -1,14 +1,17 @@
 package fun.wordle
 
-import cats.effect.Sync
+import cats.effect._
 import cats.implicits._
 import org.http4s.HttpRoutes
+import org.http4s.client._
 import org.http4s.dsl.Http4sDsl
 
-object WordleRoutes {
+import java.util.concurrent._
+import scala.concurrent.ExecutionContext
 
-  def jokeRoutes[F[_]: Sync](J: Jokes[F]): HttpRoutes[F] = {
-    val dsl = new Http4sDsl[F]{}
+object WordleRoutes {
+  def jokeRoutes[F[_] : Sync](J: Jokes[F]): HttpRoutes[F] = {
+    val dsl = new Http4sDsl[F] {}
     import dsl._
     HttpRoutes.of[F] {
       case GET -> Root / "joke" =>
@@ -19,13 +22,18 @@ object WordleRoutes {
     }
   }
 
-  def helloWorldRoutes[F[_]: Sync](H: HelloWorld[F]): HttpRoutes[F] = {
-    val dsl = new Http4sDsl[F]{}
+  def helloWorldRoutes[F[_] : Sync](H: HelloWorld[F]): HttpRoutes[F] = {
+    val httpClient: Client[IO] = JavaNetClientBuilder[IO].create
+
+    val dsl = new Http4sDsl[F] {}
     import dsl._
     HttpRoutes.of[F] {
       case GET -> Root / "hello" / name =>
         for {
           greeting <- H.hello(HelloWorld.Name(name))
+          wordle <- httpClient.expect[String]("https://www.nytimes.com/games/wordle/index.html")
+          s = name + "(" + wordle.indexOf("rupee")+ ")"
+          greeting <- H.hello(HelloWorld.Name(s))
           resp <- Ok(greeting)
         } yield resp
     }
